82188ad2ba361b7f4befb15cfbf82f18a85776da
Kai Wegner <kwnetzwelt@gmail.com>
2021-03-17 23:09:51 +0100

    stats wip added filter menu

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatsComponent implements OnInit {
 
+  public selectedStartDate:Date;
+  public selectedEndDate:Date;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public lastYearClicked()
+  {
+    let now:Date = new Date();
+
+    this.selectedStartDate = new Date(now.getFullYear()-1, 0,1);
+    this.selectedEndDate = new Date(now.getFullYear()-1, 11,31);
+  }
+  public thisYearClicked()
+  {
+    
+    let now:Date = new Date();
+
+    this.selectedStartDate = new Date(now.getFullYear(), 0,1);
+    this.selectedEndDate = new Date(now.getFullYear(), 11,31);
+  }
+  public lastMonthClicked()
+  {
+
+    let now:Date = new Date();
+
+    this.selectedStartDate = new Date(now.getFullYear(), now.getMonth()-1,1);
+    this.selectedEndDate = new Date(now.getFullYear(), now.getMonth(),0);
+  }
+  public thisMonthClicked()
+  {
+
+    let now:Date = new Date();
+
+    this.selectedStartDate = new Date(now.getFullYear(), now.getMonth(),1);
+    this.selectedEndDate = new Date(now.getFullYear(), now.getMonth()+1,0);
   }
 
 }
